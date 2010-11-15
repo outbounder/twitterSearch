4722a736fd21c96dac93b213bbe98aa2daff700e
outbounder <obiwonn@gmail.com>
2010-11-15 04:36:33 +0200

    major improvement on data presentation

--- src/www/index.js
+++ src/www/index.js
@@ -7,6 +7,7 @@
 			watchId: null,
 			refreshUrl : null,
 			counter : 0,
+			lastResultsCount: 0,
 			startWatching : function(term) {
 				this.stopWatching();
 				
@@ -17,10 +18,17 @@
 				_self.watchId = setInterval(function(){
 					_self.lastQueryDate = new Date();
 					var dataUrl = _self.getDataUrl(term);
+					console.log('here');
 					$.getJSON(dataUrl, function(data){
-						_self.refreshUrl = data.refresh_url;
-						_self.dispatchTrendChange([_self.lastQueryDate.getTime()-_self.startQueryDate.getTime(), data.results.length]);
+						console.log(data.results.length);
+						_self.dispatchTrendChange([(_self.lastQueryDate.getTime()-_self.startQueryDate.getTime())/1000, _self.lastResultsCount+data.results.length]);
 						_self.counter += 1;
+						if(data.refresh_url != _self.refreshUrl)
+							_self.lastResultsCount += data.results.length;
+						else
+							_self.lastResultsCount = 0;
+						_self.refreshUrl = data.refresh_url;
+						
 					});
 				}, 1000);
 			},
@@ -31,10 +39,12 @@
 				this.watchId = null;
 				this.counter = 0;
 				this.dispatchTrendReset();
+				this.lastResultsCount = 0;
 			},
 			getDataUrl : function(searchTerm) {
+				var d = new Date();
 				if(this.refreshUrl == null)
-					return "http://search.twitter.com/search.json?q="+searchTerm+"&since=2010-11-13&callback=?";
+					return "http://search.twitter.com/search.json?q="+searchTerm+"&since="+d.getFullYear()+"-"+d.getMonth()+"-"+d.getDate()+"&callback=?";
 				else
 					return "http://search.twitter.com/search.json"+this.refreshUrl+"&callback=?";
 			},
